5969a5cd98bc2dc12b7c6fb26675303da8c6d6bc
Ashish Singh <ashishsingh21798@gmail.com>
2023-01-23 09:19:43 +0530

    Check the randomness of the go routine

--- goroutine.go
+++ goroutine.go
@@ -18,9 +18,11 @@ func Set(ch chan<- int, v int) {
 func main() {
 	ch := make(chan int)
 	fmt.Println("Go routine start")
-	wg.Add(2)
-	go Set(ch, 10)
-	go Print(ch)
+	for i := 0; i < 10; i++ {
+		wg.Add(2)
+		go Set(ch, i)
+		go Print(ch)
+	}
 	wg.Wait()
 	fmt.Println("Go routine End")
 }
